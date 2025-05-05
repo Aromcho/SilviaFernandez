74000f27e69551189334e66fb9e54721f8f52c5e
Aromcho <barriosarom@gmail.com>
2025-05-05 18:05:04 -0300

    quite alquiler temporal de nuevo

--- Frontend/src/components/Menu/Menu.jsx
+++ Frontend/src/components/Menu/Menu.jsx
@@ -104,16 +104,6 @@ export const Menu = () => {
 >
   Lotes - Terrenos
 </Nav.Link>
-
-
-<Nav.Link
-  as={Link}
-  to="temporal"
-  className={`menu--link ${sticky || !isHome ? "black-line sticky-link" : ""}`}
->
-  alquiler temporal
-</Nav.Link>
-
 <Nav.Link
   as={Link}
   to="complejos"
